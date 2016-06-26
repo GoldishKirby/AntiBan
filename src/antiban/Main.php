1d08d89d7e8cb6a7906fc228c36527b014d14174
Kirby <goldishkirby@mailinator.com>
2016-06-26 10:04:01 +0800

    Config welp

--- src/antiban/Main.php
+++ src/antiban/Main.php
@@ -7,7 +7,19 @@ use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\utils\TextFormat;
 use pocketmine\plugin\PluginBase;
- 
-// STUFF HERE
 
-?>
+class Main extends PluginBase {
+ 
+public function onPlayerRunCommand(PlayerCommandPreprocessEvent $event){
+    $commandInfo = explode(" ", $event->getMessage());
+    $command = substr(array_shift($commandInfo), 1);
+    if($command === "ban"){
+        $players = $this->getConfig()->getNested("players");
+    }
+    foreach($players as $player){
+        if($player === $commandInfo[0]){
+            $event->setCancelled();
+        }
+    }
+}
+}
