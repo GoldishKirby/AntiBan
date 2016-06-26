52fbe416f6d249dee59558f7d5ebfa0465573a04
Kirby <goldishkirby@mailinator.com>
2016-06-26 09:58:10 +0800

    Update Main.php

--- src/antiban/Main.php
+++ src/antiban/Main.php
@@ -1,7 +1,13 @@
 <?php
-  namespace antiban;
-  
-  //Z CODE HERE
 
+namespace antiban;
+
+use pocketmine\event\player\PlayerCommandPreprocessEvent;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat;
+use pocketmine\plugin\PluginBase;
+ 
+// STUFF HERE
 
 ?>
